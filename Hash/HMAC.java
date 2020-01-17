6735ddfc32be532f6baf58e1fc41953cc13182de
DB-Hacks <Daniel.Brown.1396@gmail.com>
2020-01-16 23:05:27 -0500

    Updated HMAC.java

--- Hash/HMAC.java
+++ Hash/HMAC.java
@@ -1,8 +1,7 @@
-package hash;
+package Hash;
 
 import java.security.MessageDigest;
-
-import util.CryptoTools;
+import Utility.CryptoTools;
 
 public class HMAC {
 	public static void main(String[] args) throws Exception
@@ -13,7 +12,6 @@ public class HMAC {
 		MessageDigest md = MessageDigest.getInstance("SHA1");
 		
 		int block = 64;
-		int output = 20;
 		
 		if(ky.length > block) {
 			ky = md.digest(ky);
@@ -31,10 +29,7 @@ public class HMAC {
 			ipad[i] = 0x36;
 			opad[i] = 0x5C;
 		}
-//		for (int i = 0; i < block; i++) {
-//			ipad[i] = 0x5C;
-//			opad[i] = 0x36;
-//		}
+
 		byte[] kIpadXorResult = new byte[block];
 		for (int i = 0; i < block; i++) {
 			kIpadXorResult[i] = (byte) (ky[i] ^ ipad[i]);
@@ -47,9 +42,6 @@ public class HMAC {
 		byte[] hash1 = md.digest(CryptoTools.hexToBytes(CryptoTools.bytesToHex(ipad) + CryptoTools.bytesToHex(m)));
 		byte[] hash2 = md.digest(CryptoTools.hexToBytes(CryptoTools.bytesToHex(opad) + CryptoTools.bytesToHex(hash1)));
 		
-		System.out.println(CryptoTools.bytesToHex(hash2));
-		
-		//regular:  928844A5052EFBC4EFC61FB6C3755019BD7D6492
-		//reversed: 21845A48484E3311414EE8472724275BBC4EC534
+		System.out.println("HMAC = " + CryptoTools.bytesToHex(hash2));		//Test Case: 928844A5052EFBC4EFC61FB6C3755019BD7D6492
 	}
 }
